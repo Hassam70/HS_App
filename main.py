51b82dcd32fab5049199aaf13b6b55ff15a3ae60
alihussain <ahalihussain11@gmail.com>
2024-12-22 03:27:18 +0500

    added UserAccount & NIC schema

--- main.py
+++ main.py
@@ -1,24 +1,40 @@
 from fastapi import FastAPI
-from fastapi.params import Body
+from pydantic import BaseModel
+from enum  import Enum
+from datetime import datetime
 
 app = FastAPI()
 
+class Gender(bool, Enum):
+   MALE: "male" # type: ignore
+   FEMALE: "female" # type: ignore
 
 
-@app.get("/")
-async def read_root():
+class PrimaryModel(BaseModel):
+    created_at: datetime
+    updated_at: datetime
 
-    return {"Hello": "world!!!!"}
-
-
-
-
-@app.get("/posts")
-def root():
-    return("king kohli crisps")         
+class NIC(BaseModel):
+    nic_number: str
+    nic_expiry: str
+    nic_name: str
 
 
-@app.post("/postcreate")
-def root(payload:dict = Body(...)):
-    print(payload)
-    return("BABAR AZAM")
+class UserAccount(BaseModel):
+    account_title: str
+    account_number: str
+    is_active: bool
+    is_dormant: bool
+    is_biometric: bool
+    nic: NIC
+
+
+class User(PrimaryModel):
+    first_name: str
+    last_name: str
+    phone_number: int
+    email: str
+    gender: Gender
+    account: UserAccount
+
+
